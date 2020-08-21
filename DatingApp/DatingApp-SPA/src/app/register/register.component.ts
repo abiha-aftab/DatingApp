5b520141297fd879e243422e29028d59ef262a92
Abiha Aftab <aaftab@xavor.com>
2020-08-21 10:56:46 +0500

    section 6

--- DatingApp/DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp/DatingApp-SPA/src/app/register/register.component.ts
@@ -1,28 +1,38 @@
-import {Component, OnInit, Input,EventEmitter,Output} from '@angular/core'
-import {AuthService} from '../_services/auth.service'
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
-
 export class RegisterComponent implements OnInit {
-  @Output() cancelRegister= new EventEmitter();
-  model: any= {};
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
 
-  constructor(private authService:AuthService) { }
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit(): void {
+  // tslint:disable-next-line: typedef
+  ngOnInit() {}
+
+  // tslint:disable-next-line: typedef
+  register() {
+    this.authService.register(this.model).subscribe(
+      () => {
+        this.alertify.success('registration successful');
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
   }
-  register(){
-    this.authService.register(this.model).subscribe(()=>{
-      console.log('registeration successful');
-    },error => { console.log(error);
-    });
-  }
-  cancel(){
+
+  // tslint:disable-next-line: typedef
+  cancel() {
     this.cancelRegister.emit(false);
-    console.log("cancelled");
   }
 }
